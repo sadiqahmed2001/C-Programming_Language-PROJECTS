3edd8f88da21ec2f983fc5070707eee63e2742b6
Sadiq Ahmed <154236087+sadiqahmed2001@users.noreply.github.com>
2024-05-20 10:55:21 +0530

    Add files via upload

---
+++ All-MINI_Projects/calculator/calculator.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int main() {
+    char operator;
+    double num1, num2;
+
+    printf("Enter an operator (+, -, *, /): ");
+    scanf("%c", &operator);
+
+    printf("Enter two numbers: ");
+    scanf("%lf %lf", &num1, &num2);
+
+    switch (operator) {
+        case '+':
+            printf("%.2lf + %.2lf = %.2lf", num1, num2, num1 + num2);
+            break;
+        case '-':
+            printf("%.2lf - %.2lf = %.2lf", num1, num2, num1 - num2);
+            break;
+        case '*':
+            printf("%.2lf * %.2lf = %.2lf", num1, num2, num1 * num2);
+            break;
+        case '/':
+            if (num2 == 0) {
+                printf("Error! Division by zero.");
+            } else {
+                printf("%.2lf / %.2lf = %.2lf", num1, num2, num1 / num2);
+            }
+            break;
+        default:
+            printf("Error! Invalid operator.");
+    }
+
+    return 0;
+}
